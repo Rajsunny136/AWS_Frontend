7c443272067fb28c08c0fbdecb511c537902b62b
Rajsunny136 <rajuanneboina136@gmail.com>
2024-10-16 12:11:44 +0530

    modified

--- app/api-request/config.ts
+++ app/api-request/config.ts
@@ -23,12 +23,12 @@ if (env === "production") {
     config.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || ""; // Set your Google API key here
     config.SOCKET_IO_URL = "https://ship.genamplifysol.com";
 } else {
-    config.API_DOMAIN_URL = "http://192.168.125.200:3000"; // Development URL
+    config.API_DOMAIN_URL = "http://192.168.63.17:3000"; // Development URL
     config.GOOGLE_API_KEY = "AIzaSyA9qviqi7tO8nndT6WAP_O5qr3NrfpILl0";
-    config.SOCKET_IO_URL = "http://192.168.125.200:3000"; // Development Socket URL
+    config.SOCKET_IO_URL = "http://192.168.63.17:3000"; // Development Socket URL
 }
 
 export default config;
 
-export const origin = env === "production" ? "https://ship.genamplifysol.com" : "http://192.168.125.200:3000";
+export const origin = env === "production" ? "https://ship.genamplifysol.com" : "http://192.168.63.17:3000";
 export const userCookie = "userToken";
